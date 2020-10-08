7ff1acf6e9004861d339c91f78bff46bbc582581
Sanzid olioul <sanzid94@gmail.com>
2020-10-08 15:33:36 +0600

    First time Learning arduino and uploading 2 LED blinking One afer another code

---
+++ Two LED Blinking/Two LED Blinking.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  digitalWrite(3,LOW);
+  delay(1000);
+  digitalWrite(13,LOW);
+  digitalWrite(3,HIGH);
+  delay(1000);
+}
